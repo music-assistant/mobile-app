1fb67b1c6f2ff84714eb8f2fa541e832c0d90e86
Callum Stott <callum@seadowg.com>
2026-02-04 20:36:55 +0000

    Extract default server details to constants

--- composeApp/src/commonMain/kotlin/io/music_assistant/client/ui/compose/settings/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/io/music_assistant/client/ui/compose/settings/SettingsScreen.kt
@@ -43,6 +43,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import io.music_assistant.client.api.ConnectionInfo
+import io.music_assistant.client.api.Defaults
 import io.music_assistant.client.data.model.server.ServerInfo
 import io.music_assistant.client.data.model.server.User
 import io.music_assistant.client.ui.compose.auth.AuthenticationPanel
@@ -117,8 +118,8 @@ fun SettingsScreen(goHome: () -> Unit, exitApp: () -> Unit) {
                     .padding(horizontal = 16.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                var ipAddress by remember { mutableStateOf("homeassistant.local") }
-                var port by remember { mutableStateOf("8095") }
+                var ipAddress by remember { mutableStateOf(Defaults.URI) }
+                var port by remember { mutableStateOf(Defaults.PORT.toString()) }
                 var isTls by remember { mutableStateOf(false) }
 
                 LaunchedEffect(savedConnectionInfo) {
