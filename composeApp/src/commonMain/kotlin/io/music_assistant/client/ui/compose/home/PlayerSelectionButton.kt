c80081b527cbfea0171c38bc39acb2b400bc16c5
Callum Stott <callum@seadowg.com>
2026-03-06 15:46:02 +0000

    Use selectable group

--- composeApp/src/commonMain/kotlin/io/music_assistant/client/ui/compose/home/PlayerSelectionButton.kt
+++ composeApp/src/commonMain/kotlin/io/music_assistant/client/ui/compose/home/PlayerSelectionButton.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Speaker
@@ -22,6 +25,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -55,6 +59,7 @@ fun PlayerSelectionButton(
 
         Text(
             currentPlayer.player.displayName,
+            modifier = Modifier.padding(top = 4.dp),
             color = MaterialTheme.colorScheme.primary,
             style = MaterialTheme.typography.labelSmall
         )
@@ -82,18 +87,36 @@ private fun SelectPlayerDialog(
             modifier = Modifier.fillMaxWidth(),
             shape = RoundedCornerShape(16.dp)
         ) {
-            Column(Modifier.padding(16.dp)) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+                    .selectableGroup()
+            ) {
                 players.forEach {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
+                    val selected = it.player.id == selectedPlayer.player.id
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(56.dp)
+                            .selectable(
+                                selected = selected,
+                                onClick = { onMoveToPlayer(it.player.id) },
+                                role = Role.RadioButton
+                            ),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
                         RadioButton(
-                            selected = it.player.id == selectedPlayer.player.id,
-                            onClick = {
-                                onMoveToPlayer(it.player.id)
-                                onDismissRequest()
-                            }
+                            selected = selected,
+                            onClick = null
                         )
 
-                        Text(it.player.displayName)
+                        Text(
+                            it.player.displayName,
+                            style = MaterialTheme.typography.bodyLarge,
+                            modifier = Modifier.padding(start = 16.dp)
+                        )
                     }
                 }
             }
