e511e5c1c42adabcafcfffb30a22cebd177ca249
Andrii Mitnovych <mitnovich@gmail.com>
2026-04-01 14:25:44 -0700

    Cleaned tests

--- androidApp/src/test/kotlin/io/music_assistant/client/ui/compose/home/players/SelectPlayerDialogTest.kt
+++ androidApp/src/test/kotlin/io/music_assistant/client/ui/compose/home/players/SelectPlayerDialogTest.kt
@@ -1,11 +1,8 @@
 package io.music_assistant.client.ui.compose.home.players
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertIsEnabled
-import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import io.music_assistant.client.data.model.client.PlayerDataFixtures
 import io.music_assistant.client.ui.compose.support.inScrollable
@@ -18,38 +15,6 @@ class SelectPlayerDialogTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
-
-    @Test
-    fun `group settings not enabled for players without group children`() {
-        val selectedPlayer = PlayerDataFixtures.playerData(groupChildren = emptyList())
-        val otherPlayer =
-            PlayerDataFixtures.playerData(groupChildren = listOf(PlayerDataFixtures.bind()))
-
-        composeTestRule.setContent {
-            SelectPlayerDialog(
-                selectedPlayer = selectedPlayer,
-                listOf(selectedPlayer, otherPlayer)
-            )
-        }
-
-        composeTestRule.onNodeWithText("Group").assertIsNotEnabled()
-    }
-
-    @Test
-    fun `group settings enabled for players with groups children`() {
-        val otherPlayer = PlayerDataFixtures.playerData(groupChildren = emptyList())
-        val selectedPlayer =
-            PlayerDataFixtures.playerData(groupChildren = listOf(PlayerDataFixtures.bind()))
-
-        composeTestRule.setContent {
-            SelectPlayerDialog(
-                selectedPlayer = selectedPlayer,
-                listOf(otherPlayer, selectedPlayer)
-            )
-        }
-
-        composeTestRule.onNodeWithText("Group").assertIsEnabled()
-    }
 
     @Test
     fun `scrolls long list of players`() {
@@ -67,25 +32,5 @@ class SelectPlayerDialogTest {
         composeTestRule.inScrollable("PlayersList") {
             onNode(hasText(players.last().player.name)).assertIsDisplayed()
         }
-    }
-
-    @Test
-    fun `group button is always shown on long list of players`() {
-        val players = 0.until(25).map {
-            PlayerDataFixtures.playerData()
-        }
-
-        composeTestRule.setContent {
-            SelectPlayerDialog(
-                selectedPlayer = players[0],
-                players
-            )
-        }
-
-        composeTestRule.inScrollable("PlayersList") {
-            onNode(hasText(players.first().player.name)).assertIsDisplayed()
-        }
-
-        composeTestRule.onNodeWithText("Group").assertIsDisplayed()
     }
 }
