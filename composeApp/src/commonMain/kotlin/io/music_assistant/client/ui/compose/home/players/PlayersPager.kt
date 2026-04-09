1e02cf3951569fef72788626904cc15165612178
Callum Stott <callum@seadowg.com>
2026-04-09 13:49:37 +0100

    Extract separate composables for each mode of PlayerPager

--- composeApp/src/commonMain/kotlin/io/music_assistant/client/ui/compose/home/players/PlayersPager.kt
+++ composeApp/src/commonMain/kotlin/io/music_assistant/client/ui/compose/home/players/PlayersPager.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -107,8 +108,6 @@ internal fun PlayersPager(
             )
         }
 
-        var isQueueExpanded by remember { mutableStateOf(false) }
-
         val imageUrl = player.queueInfo?.currentItem?.track?.imageInfo?.url(serverUrl)
         val dominantColor by rememberAnimatedDominantColor(
             imageUrl = imageUrl,
@@ -116,194 +115,268 @@ internal fun PlayersPager(
         )
 
         Column(
-            Modifier.background(
-                brush = if (player.isLocal) {
-                    Brush.verticalGradient(
-                        listOf(
-                            MaterialTheme.colorScheme.surfaceContainerHigh,
-                            MaterialTheme.colorScheme.surfaceContainerLow
+            Modifier
+                .fillMaxSize()
+                .background(
+                    brush = if (player.isLocal) {
+                        Brush.verticalGradient(
+                            listOf(
+                                MaterialTheme.colorScheme.surfaceContainerHigh,
+                                MaterialTheme.colorScheme.surfaceContainerLow
+                            )
                         )
-                    )
-                } else {
-                    Brush.verticalGradient(
-                        listOf(
-                            MaterialTheme.colorScheme.surfaceContainerHigh,
-                            dominantColor.copy(alpha = 0.6f)
+                    } else {
+                        Brush.verticalGradient(
+                            listOf(
+                                MaterialTheme.colorScheme.surfaceContainerHigh,
+                                dominantColor.copy(alpha = 0.6f)
+                            )
                         )
-                    )
-                }
-            )
+                    }
+                )
         ) {
-            if (!isExpandedScreen || expanded) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    PlayerSelectionLayout(
-                        player = player,
-                        playersState = playersState,
-                        onSelectPlayer = onSelectPlayer,
-                        onGroupButton = onGroupButton
-                    )
-                }
-            }
-
-            AnimatedVisibility(
-                visible = isQueueExpanded.takeIf { expanded } != false,
-                enter = fadeIn(tween(300)) + expandVertically(tween(300)),
-                exit = fadeOut(tween(200)) + shrinkVertically(tween(300))
-            ) {
-                Box(
-                    modifier = Modifier
-                        .padding(top = 2.dp)
-                        .fillMaxWidth()
-                        .wrapContentSize()
-                        .conditional(
-                            expanded,
-                            { clickable { isQueueExpanded = false } }
-                        )
-                ) {
-                    CompactPlayerItem(
-                        item = player,
-                        playersState = playersState,
-                        serverUrl = serverUrl,
-                        playerAction = playerAction,
-                        onSelectPlayer = if (isExpandedScreen && !isQueueExpanded) onSelectPlayer else null,
-                        onGroupButton = if (isExpandedScreen && !isQueueExpanded) onGroupButton else null,
-                        showAdditionalControls = isExpandedScreen,
-                    )
-                }
-            }
-
-            Column(
-                modifier = Modifier
-                    .conditional(
-                        condition = isQueueExpanded.takeIf { expanded } == false,
-                        ifTrue = { weight(1f) },
-                        ifFalse = { wrapContentHeight() }
-                    )
-            ) {
-                AnimatedVisibility(
-                    visible = isQueueExpanded.takeIf { expanded } == false,
-                    enter = fadeIn(tween(300)) + expandVertically(tween(300)),
-                    exit = fadeOut(tween(200)) + shrinkVertically(tween(300))
-                ) {
-
-                    FullPlayerItem(
-                        modifier = Modifier.fillMaxSize(),
-                        item = player,
-                        isLocal = player.isLocal,
-                        serverUrl = serverUrl,
-                        playerAction = playerAction,
-                        onFavoriteClick = onFavoriteClick,
-                    )
-                }
-            }
-
-            if (
-                expanded
-                && player.player.isVolumeSliderAccessible
-                && player.player.currentVolume != null
-            ) {
-                if (!player.isLocal) {
-                    var currentVolume by remember(player.player.currentVolume) {
-                        mutableStateOf(player.player.currentVolume)
-                    }
-                    Row(
-                        modifier = Modifier.fillMaxWidth().height(36.dp)
-                            .padding(horizontal = 64.dp),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.spacedBy(8.dp)
-                    ) {
-                        Icon(
-                            modifier = Modifier
-                                .size(24.dp)
-                                .alpha(if (player.player.canMute) 1F else 0.5f)
-                                .clickable(enabled = player.player.canMute) {
-                                    playerAction(
-                                        player,
-                                        PlayerAction.ToggleMute(player.player.volumeMuted)
-                                    )
-                                },
-                            imageVector = if (player.player.volumeMuted)
-                                VolumeMutedIcon
-                            else
-                                VolumeIcon,
-                            contentDescription = "Volume",
-                            tint = MaterialTheme.colorScheme.primary
-                        )
-                        Slider(
-                            modifier = Modifier.weight(1f),
-                            value = currentVolume,
-                            valueRange = 0f..100f,
-                            onValueChange = {
-                                currentVolume = it
-                            },
-                            onValueChangeFinished = {
-                                playerAction(
-                                    player,
-                                    if (player.groupChildren.none { it.isBound }) {
-                                        PlayerAction.VolumeSet(currentVolume.toDouble())
-                                    } else {
-                                        PlayerAction.GroupVolumeSet(currentVolume.toDouble())
-                                    }
-                                )
-                            },
-                            thumb = {
-                                SliderDefaults.Thumb(
-                                    interactionSource = remember { MutableInteractionSource() },
-                                    thumbSize = DpSize(16.dp, 16.dp),
-                                    colors = SliderDefaults.colors()
-                                        .copy(thumbColor = MaterialTheme.colorScheme.secondary),
-                                )
-                            },
-                            track = { sliderState ->
-                                SliderDefaults.Track(
-                                    sliderState = sliderState,
-                                    thumbTrackGapSize = 0.dp,
-                                    trackInsideCornerSize = 0.dp,
-                                    drawStopIndicator = null,
-                                    modifier = Modifier.height(4.dp)
-                                )
-                            }
-                        )
-                    }
-                } else {
-                    Text(
-                        modifier = Modifier.fillMaxWidth().height(36.dp),
-                        text = "use device buttons to adjust the volume",
-                        fontSize = 12.sp,
-                        textAlign = TextAlign.Center,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.5f)
-                    )
-                }
-            }
-
-            Spacer(modifier = Modifier.fillMaxWidth().height(8.dp))
-
-            player.queue.takeIf { expanded }?.let { queue ->
-                CollapsibleQueue(
-                    modifier = Modifier
-                        .conditional(
-                            condition = isQueueExpanded,
-                            ifTrue = { weight(1f) },
-                            ifFalse = { wrapContentHeight() }
-                        ),
-                    queue = queue,
-                    isQueueExpanded = isQueueExpanded,
-                    onQueueExpandedSwitch = { isQueueExpanded = !isQueueExpanded },
-                    onGoToLibrary = onClose,
-                    serverUrl = serverUrl,
-                    queueAction = queueAction,
-                    players = allPlayers,
-                    onPlayerSelected = { playerId ->
-                        moveToPlayer(playerId)
-                    },
-                    isCurrentPage = page == playerPagerState.currentPage
+            if (expanded) {
+                ExpandedPlayerPage(
+                    player,
+                    playersState,
+                    onSelectPlayer,
+                    onGroupButton,
+                    serverUrl,
+                    playerAction,
+                    onFavoriteClick,
+                    onClose,
+                    queueAction,
+                    allPlayers,
+                    moveToPlayer,
+                    page,
+                    playerPagerState,
+                    isExpandedScreen
+                )
+            } else {
+                CompactPlayerPage(
+                    isExpandedScreen,
+                    player,
+                    playersState,
+                    onSelectPlayer,
+                    onGroupButton,
+                    serverUrl,
+                    playerAction
                 )
             }
         }
     }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun ColumnScope.ExpandedPlayerPage(
+    player: PlayerData,
+    playersState: HomeScreenViewModel.PlayersState.Data,
+    onSelectPlayer: () -> Unit,
+    onGroupButton: () -> Unit,
+    serverUrl: String?,
+    playerAction: (PlayerData, PlayerAction) -> Unit,
+    onFavoriteClick: (AppMediaItem) -> Unit,
+    onClose: () -> Unit,
+    queueAction: (QueueAction) -> Unit,
+    allPlayers: List<PlayerData>,
+    moveToPlayer: (String) -> Unit,
+    page: Int,
+    playerPagerState: PagerState,
+    isExpandedScreen: Boolean
+) {
+    var isQueueExpanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        PlayerSelectionLayout(
+            player = player,
+            playersState = playersState,
+            onSelectPlayer = onSelectPlayer,
+            onGroupButton = onGroupButton
+        )
+    }
+
+    AnimatedVisibility(
+        visible = isQueueExpanded,
+        enter = fadeIn(tween(300)) + expandVertically(tween(300)),
+        exit = fadeOut(tween(200)) + shrinkVertically(tween(300))
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(top = 2.dp)
+                .fillMaxWidth()
+                .wrapContentSize()
+                .clickable { isQueueExpanded = false }
+        ) {
+            CompactPlayerItem(
+                item = player,
+                playersState = playersState,
+                serverUrl = serverUrl,
+                playerAction = playerAction,
+                onSelectPlayer = if (isExpandedScreen && !isQueueExpanded) onSelectPlayer else null,
+                onGroupButton = if (isExpandedScreen && !isQueueExpanded) onGroupButton else null,
+                showAdditionalControls = isExpandedScreen,
+            )
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .conditional(
+                condition = !isQueueExpanded,
+                ifTrue = { weight(1f) },
+                ifFalse = { wrapContentHeight() }
+            )
+    ) {
+        AnimatedVisibility(
+            visible = !isQueueExpanded,
+            enter = fadeIn(tween(300)) + expandVertically(tween(300)),
+            exit = fadeOut(tween(200)) + shrinkVertically(tween(300))
+        ) {
+            FullPlayerItem(
+                modifier = Modifier.fillMaxSize(),
+                item = player,
+                isLocal = player.isLocal,
+                serverUrl = serverUrl,
+                playerAction = playerAction,
+                onFavoriteClick = onFavoriteClick,
+            )
+        }
+    }
+
+    if (player.player.isVolumeSliderAccessible && player.player.currentVolume != null) {
+        if (!player.isLocal) {
+            var currentVolume by remember(player.player.currentVolume) {
+                mutableStateOf(player.player.currentVolume)
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth().height(36.dp)
+                    .padding(horizontal = 64.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Icon(
+                    modifier = Modifier
+                        .size(24.dp)
+                        .alpha(if (player.player.canMute) 1F else 0.5f)
+                        .clickable(enabled = player.player.canMute) {
+                            playerAction(
+                                player,
+                                PlayerAction.ToggleMute(player.player.volumeMuted)
+                            )
+                        },
+                    imageVector = if (player.player.volumeMuted)
+                        VolumeMutedIcon
+                    else
+                        VolumeIcon,
+                    contentDescription = "Volume",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+                Slider(
+                    modifier = Modifier.weight(1f),
+                    value = currentVolume,
+                    valueRange = 0f..100f,
+                    onValueChange = {
+                        currentVolume = it
+                    },
+                    onValueChangeFinished = {
+                        playerAction(
+                            player,
+                            if (player.groupChildren.none { it.isBound }) {
+                                PlayerAction.VolumeSet(currentVolume.toDouble())
+                            } else {
+                                PlayerAction.GroupVolumeSet(currentVolume.toDouble())
+                            }
+                        )
+                    },
+                    thumb = {
+                        SliderDefaults.Thumb(
+                            interactionSource = remember { MutableInteractionSource() },
+                            thumbSize = DpSize(16.dp, 16.dp),
+                            colors = SliderDefaults.colors()
+                                .copy(thumbColor = MaterialTheme.colorScheme.secondary),
+                        )
+                    },
+                    track = { sliderState ->
+                        SliderDefaults.Track(
+                            sliderState = sliderState,
+                            thumbTrackGapSize = 0.dp,
+                            trackInsideCornerSize = 0.dp,
+                            drawStopIndicator = null,
+                            modifier = Modifier.height(4.dp)
+                        )
+                    }
+                )
+            }
+        } else {
+            Text(
+                modifier = Modifier.fillMaxWidth().height(36.dp),
+                text = "use device buttons to adjust the volume",
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.5f)
+            )
+        }
+    }
+
+    Spacer(modifier = Modifier.fillMaxWidth().height(8.dp))
+
+    CollapsibleQueue(
+        modifier = Modifier
+            .conditional(
+                condition = isQueueExpanded,
+                ifTrue = { weight(1f) },
+                ifFalse = { wrapContentHeight() }
+            ),
+        queue = player.queue,
+        isQueueExpanded = isQueueExpanded,
+        onQueueExpandedSwitch = { isQueueExpanded = !isQueueExpanded },
+        onGoToLibrary = onClose,
+        serverUrl = serverUrl,
+        queueAction = queueAction,
+        players = allPlayers,
+        onPlayerSelected = { playerId ->
+            moveToPlayer(playerId)
+        },
+        isCurrentPage = page == playerPagerState.currentPage
+    )
+}
+
+@Composable
+private fun CompactPlayerPage(
+    isExpandedScreen: Boolean,
+    player: PlayerData,
+    playersState: HomeScreenViewModel.PlayersState.Data,
+    onSelectPlayer: () -> Unit,
+    onGroupButton: () -> Unit,
+    serverUrl: String?,
+    playerAction: (PlayerData, PlayerAction) -> Unit
+) {
+    if (!isExpandedScreen) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            PlayerSelectionLayout(
+                player = player,
+                playersState = playersState,
+                onSelectPlayer = onSelectPlayer,
+                onGroupButton = onGroupButton
+            )
+        }
+    }
+
+    CompactPlayerItem(
+        item = player,
+        playersState = playersState,
+        serverUrl = serverUrl,
+        playerAction = playerAction,
+        onSelectPlayer = if (isExpandedScreen) onSelectPlayer else null,
+        onGroupButton = if (isExpandedScreen) onGroupButton else null
+    )
 }
 
 @Composable
@@ -328,8 +401,7 @@ private fun PlayersPagerContainer(
         }
 
         HorizontalPager(
-            modifier = Modifier
-                .wrapContentHeight(),
+            modifier = Modifier,
             state = playerPagerState,
             key = { page -> playerDataList.getOrNull(page)?.player?.id ?: page }
         ) { page ->
