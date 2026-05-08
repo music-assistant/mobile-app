453319c9a5100fe52489aaabfe7d1d16ae0552d9
Andrii Mitnovych <mitnovich@gmail.com>
2026-05-08 16:58:58 -0700

    Lint

--- composeApp/src/commonMain/kotlin/io/music_assistant/client/ui/compose/home/players/GroupSettingsDialog.kt
+++ composeApp/src/commonMain/kotlin/io/music_assistant/client/ui/compose/home/players/GroupSettingsDialog.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -38,7 +37,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpSize
