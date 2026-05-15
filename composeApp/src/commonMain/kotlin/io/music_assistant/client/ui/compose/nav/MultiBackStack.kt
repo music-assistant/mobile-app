e72f9a1a4ff834d68ee478f55e34b6a65c4ff413
Callum Stott <callum@seadowg.com>
2026-05-15 20:49:22 +0100

    Simplify MultiBackStack

--- composeApp/src/commonMain/kotlin/io/music_assistant/client/ui/compose/nav/MultiBackStack.kt
+++ composeApp/src/commonMain/kotlin/io/music_assistant/client/ui/compose/nav/MultiBackStack.kt
@@ -1,26 +1,21 @@
 package io.music_assistant.client.ui.compose.nav
 
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import androidx.compose.runtime.setValue
-import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.runtime.NavKey
-import androidx.navigation3.runtime.rememberDecoratedNavEntries
-import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
 
 /**
- * Allows multiple [NavBackStack] instances to be treated as one for use with components like
+ * Allows multiple back stacks to be treated as one for use with components like
  * [androidx.navigation3.ui.NavDisplay] (with [toEntries] when combined with a
  * [androidx.compose.material3.NavigationBar].
  *
- * The [NavBackStack] entries are combined to provide "exit through home" style navigation where
+ * The back stack entries are combined to provide "exit through home" style navigation where
  * navigating backways from the last element of any back stack except the first leads to the top
  * element of the first back stack.
  */
-class MultiBackStack(private val backStacks: List<NavBackStack<NavKey>>) {
+class MultiBackStack(private val backStacks: List<MutableList<NavKey>>) {
     var currentBackStack by mutableStateOf(0)
 
     private val originalItems = backStacks.map { it.toList() }
@@ -63,7 +58,6 @@ class MultiBackStack(private val backStacks: List<NavBackStack<NavKey>>) {
         }
     }
 
-    @Composable
     fun toEntries(entryProvider: (NavKey) -> NavEntry<NavKey>): List<NavEntry<NavKey>> {
         val activeBackStacks = if (currentBackStack == 0) {
             listOf(backStacks[0])
@@ -71,15 +65,8 @@ class MultiBackStack(private val backStacks: List<NavBackStack<NavKey>>) {
             listOf(backStacks[0]) + listOf(backStacks[currentBackStack])
         }
 
-        val saveableStateHolderForHome = rememberSaveableStateHolder()
-        return activeBackStacks.flatMap {
-            rememberDecoratedNavEntries(
-                backStack = it,
-                entryDecorators = listOf(
-                    rememberSaveableStateHolderNavEntryDecorator(saveableStateHolderForHome),
-                ),
-                entryProvider = entryProvider,
-            )
+        return activeBackStacks.flatMap { backStack ->
+            backStack.map { entryProvider(it) }
         }
     }
 }
