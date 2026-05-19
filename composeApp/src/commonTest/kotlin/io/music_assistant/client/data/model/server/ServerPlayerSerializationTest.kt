8aa540a29f634600bb1edce90a7f7cb2752f2b28
Andrii Mitnovych <mitnovich@gmail.com>
2026-05-19 06:19:48 -0700

    Fix test

--- composeApp/src/commonTest/kotlin/io/music_assistant/client/data/model/server/ServerPlayerSerializationTest.kt
+++ composeApp/src/commonTest/kotlin/io/music_assistant/client/data/model/server/ServerPlayerSerializationTest.kt
@@ -25,7 +25,7 @@ import kotlin.test.assertTrue
  *    `coerceInputValues = true` in [myJson] for unknown variants.
  */
 class ServerPlayerSerializationTest {
-    private val playerFactory = PlayerFactory()
+    private val playerFactory = PlayerFactory(FakeClient())
 
     @Test
     fun deserializesWithOnlyPlayerIdPresent() {
