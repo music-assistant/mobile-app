8cf474292b7c77577055897794fd3d4ec818ab78
Callum Stott <callum@seadowg.com>
2026-06-01 14:10:45 +0100

    Add test for opposite case

--- androidApp/src/test/kotlin/io/music_assistant/client/regression/AndroidBackGestureTest.kt
+++ androidApp/src/test/kotlin/io/music_assistant/client/regression/AndroidBackGestureTest.kt
@@ -11,13 +11,13 @@ import io.music_assistant.client.support.ServerPlayerFixtures
 import io.music_assistant.client.support.launchLoggedInApp
 import io.music_assistant.client.support.pages.assertOnPage
 import io.music_assistant.client.support.pages.expandPlayer
+import io.music_assistant.client.support.pages.playMedia
 import io.music_assistant.client.support.rules.createTestRuleChain
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.java.KoinJavaComponent.inject
 import org.robolectric.annotation.Config
-import kotlin.getValue
 
 @RunWith(AndroidJUnit4::class)
 @Config(qualifiers = Qualifiers.MEDIUM_PHONE)
@@ -39,18 +39,37 @@ class AndroidBackGestureTest {
         val player = ServerPlayerFixtures.player()
         serviceClient.addPlayers(player)
 
-        val homePage = launchLoggedInApp(composeTestRule, serviceClient)
-        val itemDetailsPage = homePage
+        val itemDetailsPage = launchLoggedInApp(composeTestRule, serviceClient)
             .clickOnMedia(album)
-
         itemDetailsPage
             .clickPlay()
             .expandPlayer(player.displayName, playing = true, item = track.name)
 
+        androidBack()
+        itemDetailsPage.assertOnPage()
+    }
+
+    @Test
+    fun `using back while on expanded player doesn't close app`() {
+        val album = ServerMediaItemFixtures.album()
+        val track = ServerMediaItemFixtures.track(album = album)
+        serviceClient.addToLibrary(track)
+
+        val player = ServerPlayerFixtures.player()
+        serviceClient.addPlayers(player)
+
+        val homePage = launchLoggedInApp(composeTestRule, serviceClient)
+        homePage
+            .playMedia(track)
+            .expandPlayer(player.displayName, playing = true, item = track.name)
+
+        androidBack()
+        homePage.assertOnPage()
+    }
+
+    private fun androidBack() {
         composeTestRule.activityRule.scenario.onActivity { activity ->
             activity.onBackPressedDispatcher.onBackPressed()
         }
-
-        itemDetailsPage.assertOnPage()
     }
 }
