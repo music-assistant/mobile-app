eb006e778e354673bfaa4df37ec0e73b9c1ff359
Callum Stott <callum@seadowg.com>
2026-06-19 08:34:01 +0100

    Extract GridButton from library

--- composeApp/src/commonMain/kotlin/io/music_assistant/client/ui/compose/library/LibraryScreen.kt
+++ composeApp/src/commonMain/kotlin/io/music_assistant/client/ui/compose/library/LibraryScreen.kt
@@ -1,27 +1,20 @@
 package io.music_assistant.client.ui.compose.library
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Tune
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarState
@@ -32,13 +25,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import io.music_assistant.client.ui.compose.common.GridButton
 import io.music_assistant.client.ui.compose.nav.ScreenState
 import io.music_assistant.client.ui.compose.nav.TopBarLayout
 import io.music_assistant.client.utils.libraryItemMinWidth
@@ -114,35 +106,12 @@ private fun LibraryGrid(
         contentPadding = paddingValues,
     ) {
         items(libraryCategories) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clickable(onClick = { onCategoryClick(it) })
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(MaterialTheme.colorScheme.primaryContainer)
-                    .padding(16.dp),
-            ) {
-                Row(
-                    modifier = Modifier.height(width / ITEM_HEIGHT_RATIO),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                ) {
-                    Icon(
-                        modifier = Modifier.size(30.dp),
-                        imageVector = it.icon(),
-                        contentDescription = it.name,
-                        tint = MaterialTheme.colorScheme.onPrimaryContainer,
-                    )
-
-                    Text(
-                        text = stringResource(it.stringResource()),
-                        style = MaterialTheme.typography.titleMedium,
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                        color = MaterialTheme.colorScheme.onPrimaryContainer,
-                    )
-                }
-            }
+            GridButton(
+                modifier = Modifier.fillMaxWidth().height(width / ITEM_HEIGHT_RATIO),
+                text = stringResource(it.stringResource()),
+                icon = it.icon(),
+                onClick = { onCategoryClick(it) },
+            )
         }
     }
 }
@@ -171,7 +140,7 @@ class LibraryScreenState(
     }
 }
 
-private const val ITEM_HEIGHT_RATIO = 4
+private const val ITEM_HEIGHT_RATIO = 3
 
 @Preview
 @Composable
